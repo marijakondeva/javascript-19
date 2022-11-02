3bf680636cb3046d1a0c764f6b9210766b2d9f93
marijakondeva <114948207+marijakondeva@users.noreply.github.com>
2022-11-02 22:02:31 +0100

    Add files via upload

---
+++ javascript-19/cass 1/prv.js
@@ -0,0 +1,13 @@
+console.log('Hello Semos !')
+
+console.warn('This is a warning!')
+
+console.error('FATAL ERROR')
+
+//komentar vo edna linija
+
+/*
+
+komentar so povekjcasse 
+linii
+*/
---
+++ javascript-19/cass 1/variables.js
@@ -0,0 +1,38 @@
+// program 1
+
+// var - kluchno zborce za definiranje / deklariranje na globalni promeni
+// let - kluchno zborce za definiranje na lokalna promenliva
+//const - definiranje na konstanti
+let username ='Alek' , emailAdrdress = 'test@test.com'
+
+//camel-case namig convention
+let emailAddress ='test@test.com'
+
+console.log(username)
+
+// so operatorot "=" zadavame vrednost na promenlivi
+username = 'Marija'
+//username = '100'
+//username = 'Boris'
+
+// so console.log gi pecatime vrednostite na promenlivite
+console.log(username)
+
+const a = 5;
+const b = a+1;
+const zbir = a + b
+console.log(zbir)
+
+// string concatenation (spojuvanje na tekst)
+console.log(username + emailAddress)
+
+// PAZETE OVA E TEKDT, NE E BROJ !
+const number = '10';
+//let test;
+//console.log(number + zbir + test)
+
+//NULL e prazna vrednost
+let test = NaN;
+let empty = null;
+let testt = '.....' ;
+console.log(test)
